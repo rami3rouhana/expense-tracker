c666361b3ae4532aa0b1575bc85d23c446c1acf4
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-08-24 14:57:51 +0300

    Adding MongoDB Connection

--- expense-tracker-nodejs/server.js
+++ expense-tracker-nodejs/server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db')
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const transactions = require('./routes/transactions')
 
 const app = express();
 
-app.use('/api/v1/transacations',transactions);
+app.use('/api/v1/transacations', transactions);
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV || "production"} mode on port ${PORT}`.green.bold))
