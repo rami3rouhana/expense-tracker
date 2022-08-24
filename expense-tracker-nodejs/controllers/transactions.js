6736fa3305285410f76b18a2c3c98221773a4978
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-08-24 14:57:31 +0300

    Adding Routes

---
+++ expense-tracker-nodejs/controllers/transactions.js
@@ -0,0 +1,18 @@
+// @desc    Get all transactions
+// @route   Get api/v1/transactions
+// @access  Public
+exports.getTransactions  = (req, res, next) => {
+    res.send('GET transactions');
+}
+// @desc    Add transaction
+// @route   Post api/v1/transactions
+// @access  Public
+exports.addTransaction  = (req, res, next) => {
+    res.send('POST transaction');
+}
+// @desc    Delete transaction
+// @route   DELETE api/v1/transactions/:id  
+// @access  Public
+exports.deleteTransaction  = (req, res, next) => {
+    res.send(`Delete Transaction ${req.id}`);
+}
