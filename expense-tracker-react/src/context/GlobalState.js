3d5720101097973a62355032a3bbe9298ea50166
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-08-25 14:15:57 +0300

    Connecting The Backend

--- expense-tracker-react/src/context/GlobalState.js
+++ expense-tracker-react/src/context/GlobalState.js
@@ -1,8 +1,11 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from './AppReducer'
+import axios from 'axios'
 
 const intialeState = {
-    transactions: []
+    transactions: [],
+    error: null,
+    loading: true
 }
 
 export const GlobalContext = createContext(intialeState);
@@ -10,23 +13,68 @@ export const GlobalContext = createContext(intialeState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, intialeState)
 
-    function deleteTransaction(id) {
-        dispatch({
-            type: 'DELETE_TRANSACTION',
-            payload: id
-        })
+    async function getTransactions() {
+        try {
+            const res = await axios.get('/v1/transacations')
+
+            dispatch({
+                type: 'GET_TRANSACTIONS',
+                payload: res.data.data
+            })
+
+        } catch (err) {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.data.error
+            })
+        }
     }
 
-    function addTransaction(transaction) {
-        dispatch({
-            type: 'ADD_TRANSACTION',
-            payload: transaction
-        })
+    async function deleteTransaction(id) {
+        try {
+            const res = await axios.delete(`/v1/transacations/${id}`)
+
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                payload: id
+            })
+
+        } catch (err) {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.data.error
+            })
+        }
+    }
+
+    async function addTransaction(transaction) {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.post(`/v1/transacations/`, transaction, config)
+
+            dispatch({
+                type: 'ADD_TRANSACTION',
+                payload: res.data.data
+            })
+
+        } catch (err) {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.data.error
+            })
+        }
     }
 
     return (
         <GlobalContext.Provider value={{
             transactions: state.transactions,
+            error: state.error,
+            loading: state.loading,
+            getTransactions,
             deleteTransaction,
             addTransaction
         }}>
